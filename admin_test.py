723c37640a4dae0c9906298ef461c090b7635ce0
antonzajmist <fakafaka555@gmail.com>
2022-02-17 21:52:38 +0300

    fix2

--- admin_test.py
+++ admin_test.py
@@ -4,11 +4,13 @@ import time
 
 def test_admin_page(main_page):
     admin_page = main_page.open_admin_page()
+    admin_page.admin_login()
     assert admin_page.site_text().text == 'Django administration'
 
 
 def test_add_group(main_page):
     admin_page = main_page.open_admin_page()
+    admin_page.admin_login()
     admin_page.open_groups()
     assert admin_page.open_groups.text == 'test_group'
 
--- page_object/admin_page.py
+++ page_object/admin_page.py
@@ -1,5 +1,7 @@
 from page_object.base_page import BasePage
 from page_object.admin_page_locators import AdminPageLocators
+from page_object.login_page_locators import LoginPageLocators
+from constants import ADMIN_LOGIN, ADMIN_PASSWORD
 
 
 class AdminPage(BasePage):
@@ -11,6 +13,14 @@ class AdminPage(BasePage):
         groups_button = self.find_element(AdminPageLocators.GROUPS_BUTTON)
         groups_button.clcik()
         return self.find_element(AdminPageLocators.TEXT_ADD_GROUP_1)
+
+    def admin_login(self):
+        username = self.find_element(LoginPageLocators.USERNAME_INPUT_LOCATORS)
+        username.send_keys(ADMIN_LOGIN)
+        password = self.find_element(LoginPageLocators.PASSWORD_INPUT_LOCATORS)
+        password.send_keys(ADMIN_PASSWORD)
+        button = self.find_element(LoginPageLocators.LOG_IN_BUTTON)
+        button.click()
 
 
 
